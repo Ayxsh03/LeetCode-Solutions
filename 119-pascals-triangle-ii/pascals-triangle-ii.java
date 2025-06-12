91d85617d3a25b1e5dc870817c9063884cf312f6
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-06-12 10:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (11.43%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,19 +1,13 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> ans = new ArrayList<>();
-        for(int col = 0; col <= rowIndex; col++){ //0th->1 1st-> 2...3rd->4
-            ans.add(nCr(rowIndex,col));
+        List<Integer> arr = new ArrayList<>();
+        long ans=1;
+        arr.add(1);
+        for(int col = 0; col < rowIndex; col++){ //0th->1 1st-> 2...3rd->4
+            ans = ans*(rowIndex-col);
+            ans = ans/(col+1);
+            arr.add((int)ans);
         }
-        return ans;
-    }
-
-    public static int nCr(int n, int r) {
-        long res = 1;
-        if(r>n/2) r = n-r;
-        for (int i = 0; i < r; i++) {
-            res = res * (n - i);
-            res = res / (i + 1);
-        }
-        return (int)res;
+        return arr;
     }
 }
