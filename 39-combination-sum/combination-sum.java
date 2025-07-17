ea595e6bcbed3ae4e243b46d9bbca8abced9f7aa
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-07-18 00:06:24 +0530

    Time: 1 ms (99.90%) | Memory: 44.3 MB (98.81%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,25 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
-        backtrack(list, new ArrayList<>(), nums, target, 0);
+        findCombSum(0, target, new ArrayList<>(), list, candidates);
         return list;
     }
 
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
-        if (remain < 0) return;
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
-        else {
-            for (int i = start; i < nums.length; i++) {
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-                tempList.remove(tempList.size() - 1);
-            }
+    public static void findCombSum(int i, int target, List<Integer> ans, List<List<Integer>> list, int[] candidates){
+        if(i == candidates.length){
+            if(target == 0) list.add(new ArrayList<>(ans));
+            return;
         }
+        
+        // Take the element if it doesn't exceed target
+        if (candidates[i] <= target) {
+            ans.add(candidates[i]);
+            findCombSum(i, target - candidates[i], ans, list, candidates);
+            ans.remove(ans.size() - 1); // backtrack
+        }
+
+        // Not take the element
+        findCombSum(i + 1, target, ans, list, candidates);
+        
     }
 }
