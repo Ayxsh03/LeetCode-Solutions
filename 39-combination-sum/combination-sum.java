0c9a7fbacf298f7132c75574f7a2418a1498f679
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-07-18 00:27:08 +0530

    Time: 2 ms (82.99%) | Memory: 44.8 MB (31.35%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,11 +1,13 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
         List<List<Integer>> list = new ArrayList<>();
         findCombSum(0, target, new ArrayList<>(), list, candidates);
         return list;
     }
 
-    public static void findCombSum(int i, int target, List<Integer> ans, List<List<Integer>> list, int[] candidates){
+    public static void findCombSum(int i, int target, List<Integer> ans, List<List<Integer>> 
+    list, int[] candidates){
         if(i == candidates.length){
             if(target == 0) list.add(new ArrayList<>(ans));
             return;
@@ -20,6 +22,5 @@ class Solution {
 
         // Not take the element
         findCombSum(i + 1, target, ans, list, candidates);
-        
     }
 }
