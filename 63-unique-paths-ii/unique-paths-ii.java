3a24d1161455d2a4d2cc7052ecf5bb9acfae73a6
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-07-27 13:11:11 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (49.31%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,23 +1,25 @@
-class Solution {
+class Solution { //Space optimisation
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if (obstacleGrid[0][0] == 1) return 0;
 
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] dp = new int[m][n];
-
-        dp[0][0] = 1;
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[] prev = new int[n];
         for (int i = 0; i < m; i++) {
+            int[] curr = new int[n];
             for (int j = 0; j < n; j++) {
-                if(i == 0 && j == 0) continue;
-                if(obstacleGrid[i][j] != 1){
-                    int up = (i > 0) ? dp[i - 1][j] : 0;
-                    int left = (j > 0) ? dp[i][j - 1] : 0;
-                    dp[i][j] = up + left; 
+                if(i == 0 && j == 0){
+                    curr[0] = 1;
+                    continue;
                 }
-                else dp[i][j] = 0; 
+                if(obstacleGrid[i][j] != 1){
+                    int up = (i > 0) ? prev[j] : 0;
+                    int left = (j > 0) ? curr[j - 1] : 0;
+                    curr[j] = up + left; 
+                }
+                else curr[j] = 0; 
             }
+            prev = curr;
         }
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 }
