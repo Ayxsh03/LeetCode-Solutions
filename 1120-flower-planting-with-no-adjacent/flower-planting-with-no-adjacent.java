9eb8ec51691fb9c5c18c06e1bbca930984227305
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-08-29 12:17:56 +0530

    Time: 11 ms (94.96%) | Memory: 50.6 MB (85.79%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.java
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[] gardenNoAdj(int n, int[][] paths) {
-        List<Integer>[] g = new ArrayList[n];
-        for (int i = 0; i < n; i++) g[i] = new ArrayList<>();
-        for (int[] p : paths) {
-            int a = p[0] - 1, b = p[1] - 1;  // make 0-indexed
-            g[a].add(b);
-            g[b].add(a);
-        }
-
-        int[] color = new int[n]; // 0 means uncolored
+        List<Integer>[] adj = new ArrayList[n];
         for (int i = 0; i < n; i++) {
-            boolean[] used = new boolean[5]; // indices 1..4
-            for (int nei : g[i]) used[color[nei]] = true; // 0 is fine
-            for (int c = 1; c <= 4; c++) if (!used[c]) { color[i] = c; break; }
+            adj[i] = new ArrayList<>();
         }
-        return color;
+        for (int[] path : paths) {
+            int u = path[0] - 1;
+            int v = path[1] - 1;
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            boolean[] used = new boolean[5];
+            for (int neighbor : adj[i]) {
+                if (ans[neighbor] != 0) {
+                    used[ans[neighbor]] = true; 
+                }
+            }
+            for (int type = 1; type <= 4; type++) {
+                if (!used[type]) {
+                    ans[i] = type;
+                    break;
+                }
+            }
+        }
+        return ans;
     }
 }
