094ba43f1512376448230b4b586c1448d34dbfad
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-09-13 23:04:10 +0530

    Time: 3 ms (60.85%) | Memory: 43 MB (24.31%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -1,18 +1,16 @@
 class Solution {
-    public int maxFreqSum(String s) {
-        HashMap<Character, Integer> map1 = new HashMap<>(); // vowel frequency
-        HashMap<Character, Integer> map2 = new HashMap<>(); // consonant frequency
-        
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') map1.put(ch, map1.getOrDefault(ch, 0) + 1);
-            else map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+    public int maxFreqSum(String s){
+        Map<Character, Integer> mp = new HashMap<>();
+        for (char ch : s.toCharArray()) mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+
+        int vowel = 0;
+        int consonant = 0;
+        for(char c = 'a'; c <= 'z'; c++){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                vowel = Math.max(vowel, mp.getOrDefault(c, 0));
+            }
+            else consonant = Math.max(consonant, mp.getOrDefault(c, 0));
         }
-        
-        int maxVowel = 0, maxCons = 0;
-        for (int freq : map1.values()) maxVowel = Math.max(maxVowel, freq);
-        for (int freq : map2.values()) maxCons = Math.max(maxCons, freq);
-        
-        return maxVowel + maxCons;
+        return vowel + consonant;
     }
 }
