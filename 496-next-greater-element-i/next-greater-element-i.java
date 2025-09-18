a5fccd8e885536767cb64aef1a03ae7b712e4672
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-09-18 14:00:11 +0530

    Time: 4 ms (34.93%) | Memory: 44 MB (48.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,26 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] NGE = new int[nums1.length];
-        Arrays.fill(NGE, -1);  // O(n2)
 
-        for (int i = 0; i < nums1.length; i++) { // O(n2)
-            //index of n2[i] in n1
-            int index = -1;
-            for (int k = 0; k < nums2.length; k++) { // O(n1)
-                if (nums2[k] == nums1[i]) {
-                    index = k;
-                    break;
-                }
-            }
-
-            //next greater element to the right in n1
-            for (int j = index + 1; j < nums2.length; j++) { // O(n1)
-                if (nums2[j] > nums2[index]) {
-                    NGE[i] = nums2[j];
-                    break;
-                }
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
+            while(!st.isEmpty() && st.peek() <= num) st.pop();
+            if (st.isEmpty()) map.put(num, -1);
+            else map.put(num, st.peek());
+            st.push(num);
         }
-        return NGE;
+        // Build answer for nums1
+        int[] nge = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            nge[i] = map.get(nums1[i]);
+        }
+        
+        return nge;
     }
 }
