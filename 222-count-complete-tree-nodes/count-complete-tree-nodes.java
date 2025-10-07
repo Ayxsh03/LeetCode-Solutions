67d1d03411f7fdbdb29eba0a688610ac034da6d2
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-10-07 10:46:04 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (39.16%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -1,13 +1,28 @@
 class Solution {
-    public int countNodes(TreeNode root){
-        return dfs(root, 0);
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        int lh = left_height(root);
+        int rh = right_height(root);
+
+        if (lh == rh) return (1 << lh) - 1;  // (2^lh - 1)
+        else return 1 + countNodes(root.left) + countNodes(root.right);
     }
 
-    int dfs(TreeNode node, int count) {
-        if (node == null) return count;
-        count++;
-        count = dfs(node.left, count);
-        count = dfs(node.right, count);
+    int left_height(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.left;
+        }
+        return count;
+    }
+    int right_height(TreeNode root) {
+        int count = 0;
+        while (root != null) {
+            count++;
+            root = root.right;
+        }
         return count;
     }
 }
