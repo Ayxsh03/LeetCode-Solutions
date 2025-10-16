bfbcb4a97adbe94b939de9420cf7875b91f047da
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-10-16 19:29:13 +0530

    Time: 1 ms (44.88%) | Memory: 44.7 MB (97.65%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,19 @@
 class Solution {
-    public int[] dx = {-1, 0, 1, 0};
-    public int[] dy = {0, 1, 0, -1};
-    
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int initial = image[sr][sc];
         if (initial == color) return image;
-
+        
         dfs(sr, sc, image, color, initial);
         return image;
     }
 
     private void dfs(int row, int col, int[][] image, int color, int initial) {
         image[row][col] = color;
+        int[] deltaRow = {-1, 0, 1, 0};
+        int[] deltaCol = {0, 1, 0, -1};
         for (int i = 0; i < 4; i++) {
-            int newRow = row + dx[i];
-            int newCol = col + dy[i];
+            int newRow = row + deltaRow[i];
+            int newCol = col + deltaCol[i];
             if (isValid(newRow, newCol, image, initial)) {
                 dfs(newRow, newCol, image, color, initial);
             }
