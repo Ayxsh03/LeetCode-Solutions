daddeb8b31ce0ab563bf15dafd7c10f154c788da
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2025-10-28 00:19:37 +0530

    Time: 9 ms (77.98%) | Memory: 44.7 MB (46.27%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,50 +1,44 @@
 class Solution {
-    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        // first convert graph to adjacency list representation
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] edge : edges) {
-            int node1 = edge[0], node2 = edge[1], distance = edge[2];
-            graph.get(node1).add(new int[]{node2, distance});
-            graph.get(node2).add(new int[]{node1, distance});
+    public int findTheCity(int n, int[][] edges, int distanceThreshold){
+        int dist[][] = new int[n][n];
+        for(int i = 0; i < n; i++){
+          for(int j = 0; j < n; j++){
+            if(i == j) dist[i][j]=0;
+            else dist[i][j] = Integer.MAX_VALUE;
+          }
         }
 
-        int minimum_number = n;
-        int res = -1;
+        for(int i = 0; i<edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
 
-        for (int source = 0; source < n; source++) {
-            int neighbors = get_number_of_neighbors_in_distance(graph, source, n, distanceThreshold);
-            // we iterate source from smaller to bigger this ensures that we choose node with greater value if they have equal number of neighbors
-            if (neighbors <= minimum_number) {
-                minimum_number = neighbors;
-                res = source;
-            }
+            dist[u][v] = wt;
+            dist[v][u] = wt;
         }
 
-        return res;
-    }
-
-    private int get_number_of_neighbors_in_distance(List<List<int[]>> graph, int source, int n, int distanceThreshold) {
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        minHeap.add(new int[]{0, source}); // distance to node itself is 0
-        Set<Integer> visited = new HashSet<>();
-
-        while (!minHeap.isEmpty()) {
-            int[] top = minHeap.poll();
-            int distance_to_this_node = top[0], cur_node = top[1];
-            if (!visited.contains(cur_node)) {
-                visited.add(cur_node);
-                for (int[] neighbor : graph.get(cur_node)) {
-                    int distance_from_source = distance_to_this_node + neighbor[1];
-                    if (distance_from_source <= distanceThreshold) { // ensure that we're allowed to go to this node
-                        minHeap.add(new int[]{distance_from_source, neighbor[0]});
-                    }
+        for(int via = 0; via < n; via++){
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < n; j++){  
+                    if(dist[i][via] < 100000000 && dist[via][j] < 100000000)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][via] + dist[via][j]);
                 }
             }
         }
-        // actually you can return visited.size() and with math there will be nothing wrong but actually we have visited.size() - 1 neighbors since we're not neighbor of ourselves
-        return visited.size() - 1;
+
+        int city = -1;
+        int minCityCount = n;
+
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if(dist[i][j]<=distanceThreshold) count++;
+            }
+            if(minCityCount >= count){
+                minCityCount = count;
+                city = i;
+            }
+        }
+        return city;
     }
 }
