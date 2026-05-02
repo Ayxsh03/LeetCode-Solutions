8c4173a85aecc7b2240abe0eccd55fe64224d4c7
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-05-02 11:29:27 +0530

    Time: 4 ms (59.28%) | Memory: 42 MB (82.58%) - LeetSync

--- 804-rotated-digits/rotated-digits.java
+++ 804-rotated-digits/rotated-digits.java
@@ -1,22 +1,25 @@
 class Solution {
     public int rotatedDigits(int n) {
         int count = 0;
-        Set<Integer> badset = new HashSet<>(Arrays.asList(3, 4, 7));
-        Set<Integer> goodset = new HashSet<>(Arrays.asList(2, 5, 6, 9));
-        for(int i = 1; i <= n; i++){
+        int[] map = {0,1,2,-1,-1,5,6,-1,8,9};
+
+        for (int i = 1; i <= n; i++) {
             int num = i;
             boolean bad = false;
-            boolean change = false;
-            while(num > 0){
-                int last = num % 10;
-                if(badset.contains(last)){
+            boolean changed = false;
+
+            while (num > 0) {
+                int digit = num % 10;
+                if (map[digit] == -1) { // invalid
                     bad = true;
                     break;
                 }
-                if(goodset.contains(last)) change = true;
-                num = num/10;
+                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                    changed = true;
+                }
+                num /= 10;
             }
-            if(!bad && change) count++;
+            if (!bad && changed) count++;
         }
         return count;
     }
