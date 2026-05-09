e2aba90c16c28b3b623203a56b9f5cbab0121ec9
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-05-09 13:36:23 +0530

    Time: 8 ms (48.03%) | Memory: 124.5 MB (86.61%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,27 +1,58 @@
 class Solution {
-    public char[][] rotateTheBox(char[][] box) {
-        int r = box.length, c = box[0].length;
-        char[][] box2 = new char[c][r];
-        
-        for(int i = 0; i<r; ++i){
-            int empty = c-1;
-            for(int j = c-1; j>=0; --j){
-                if(box[i][j] == '*'){
-                    empty = j-1;
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+
+        // Apply gravity row by row
+        for (int row = 0; row < m; row++) {
+
+            // Rightmost position where the next stone can fall
+            int empty = n - 1;
+
+            // Traverse from right to left
+            for (int col = n - 1; col >= 0; col--) {
+
+                // Obstacle blocks stones
+                // So next valid landing position becomes just before obstacle
+                if (boxGrid[row][col] == '*') {
+
+                    empty = col - 1;
                 }
-                if(box[i][j] == '#'){
-                    box[i][j] = '.';
-                    box[i][empty] = '#';
-                    --empty;
+
+                // Found a stone
+                else if (boxGrid[row][col] == '#') {
+
+                    // Remove stone from current position
+                    boxGrid[row][col] = '.';
+
+                    // Drop stone at the furthest possible right position
+                    boxGrid[row][empty] = '#';
+
+                    // Next stone must land one position before
+                    empty--;
                 }
             }
         }
-        
-        for(int i = 0; i<r; ++i){
-            for(int j = c-1; j>=0; --j)
-                box2[j][r-i-1] = box[i][j];
+
+        // Rotate box 90 degree clockwise
+        return rotate(boxGrid);
+    }
+
+    public char[][] rotate(char[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        // Rotated matrix dimensions become n x m
+        char[][] rotated = new char[n][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // Move current cell to its rotated position
+                rotated[j][m - 1 - i] = matrix[i][j];
+            }
         }
-        
-        return box2;
+        return rotated;
     }
 }
