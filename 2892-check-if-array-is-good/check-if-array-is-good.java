ecb2d7a76002dd9b62def3af820a35baf8918bf6
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-05-14 13:29:02 +0530

    Time: 5 ms (48.38%) | Memory: 45.1 MB (31.81%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -1,41 +1,12 @@
 class Solution {
     public boolean isGood(int[] nums) {
-        int[] arr = getMaxAndCount(nums);
-
-        int max = arr[0];
-        int count = arr[1];
+        Arrays.sort(nums);
         int n = nums.length;
-
-        // size should be max + 1
-        // max should appear twice
-        if(n != max + 1 || count != 2) return false;
-        int[] freq = new int[max + 1];
-        
-        for(int num : nums){
-            // invalid number
-            if(num < 1 || num > max) return false;
-            freq[num]++;
-        }
-
-        // 1 to max-1 should appear once
-        for(int i = 1; i < max; i++){
-            if(freq[i] != 1) return false;
-        }
-
-        // max should appear twice
-        return freq[max] == 2;
-    }
-
-    public int[] getMaxAndCount(int[] arr){
-        int max = arr[0];
-        int count = 1;
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                count = 1;
+        for(int i = 0; i < n - 1; i++){
+            if(nums[i] != i + 1){
+                return false;
             }
-            else if(arr[i] == max) count++;
         }
-        return new int[]{max, count};
+        return nums[n - 1] == n - 1;
     }
 }
