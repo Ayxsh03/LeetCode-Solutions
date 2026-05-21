72e248d159c58df728657d98c42ceda2898af172
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-05-21 19:06:49 +0530

    Time: 262 ms (16.62%) | Memory: 105.5 MB (5.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,11 +1,18 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int maxLen = 0;
-        int start = -1;
+    int[][] memo;
 
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i; j < s.length(); j++){
-                if(isPallindrome(s, i, j)){
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        memo = new int[n][n];
+
+        for(int[] row : memo) Arrays.fill(row, -1);
+
+        int maxLen = 0;
+        int start = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(isPalindrome(s, i, j)){
                     if(j - i + 1 > maxLen){
                         maxLen = j - i + 1;
                         start = i;
@@ -13,15 +20,19 @@ class Solution {
                 }
             }
         }
-        return s.substring(start, start + maxLen); // start , end
+        return s.substring(start, start + maxLen);
     }
 
-    public boolean isPallindrome(String s, int i, int j){
+    public boolean isPalindrome(String s, int i, int j){
         if(i >= j) return true;
 
+        if(memo[i][j] != -1) return memo[i][j] == 1;
+
         if(s.charAt(i) == s.charAt(j)){
-            return isPallindrome(s, i + 1, j - 1);
+            memo[i][j] = isPalindrome(s, i + 1, j - 1) ? 1 : 0;
         }
-        return false;
+        else memo[i][j] = 0;
+
+        return memo[i][j] == 1;
     }
 }
