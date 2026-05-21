cc0a9d211017614d51282ee51ac7a0d806dff116
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-05-21 19:01:29 +0530

    Time: 1959 ms (5.96%) | Memory: 43.3 MB (94.52%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,27 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
+        int maxLen = 0;
+        int start = -1;
 
-        int maxLen = 1;
-        String maxStr = s.substring(0, 1);
-        s = "#" + s.replaceAll("", "#") + "#";
-        int[] dp = new int[s.length()];
-        int center = 0;
-        int right = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            if (i < right) {
-                dp[i] = Math.min(right - i, dp[2 * center - i]);
-            }
-
-            while (i - dp[i] - 1 >= 0 && i + dp[i] + 1 < s.length() && s.charAt(i - dp[i] - 1) == s.charAt(i + dp[i] + 1)) {
-                dp[i]++;
-            }
-
-            if (i + dp[i] > right) {
-                center = i;
-                right = i + dp[i];
-            }
-
-            if (dp[i] > maxLen) {
-                maxLen = dp[i];
-                maxStr = s.substring(i - dp[i], i + dp[i] + 1).replaceAll("#", "");
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i; j < s.length(); j++){
+                if(isPallindrome(s, i, j)){
+                    if(j - i + 1 > maxLen){
+                        maxLen = j - i + 1;
+                        start = i;
+                    }
+                }
             }
         }
+        return s.substring(start, start + maxLen); // start , end
+    }
 
-        return maxStr;
+    public boolean isPallindrome(String s, int i, int j){
+        if(i >= j) return true;
+
+        if(s.charAt(i) == s.charAt(j)){
+            return isPallindrome(s, i + 1, j - 1);
+        }
+        return false;
     }
 }
