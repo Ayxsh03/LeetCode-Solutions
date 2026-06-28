c971d556c836edd0c14d394f12cb165cef5c3923
AYUSH SAI <152642328+Ayxsh03@users.noreply.github.com>
2026-06-28 12:37:17 +0530

    Time: 109 ms (9.44%) | Memory: 72.5 MB (29.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,35 +1,25 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n = nums.length;
-
         int totalSum = 0;
-        for(int i = 0; i < n; i++) totalSum += nums[i];
+        for (int num : nums) totalSum += num;
+        if (totalSum % 2 != 0) return false;
+
         int target = totalSum / 2;
-        if (totalSum % 2 != 0) return false; //Cant partition odd length arr
+        Boolean[][] memo = new Boolean[nums.length][target + 1];
 
-        int dp[][] = new int[n][target + 1];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < target + 1; j++){
-                dp[i][j] = -1;
-            }
-        }
-
-        return f(n - 1, target, nums, dp);
+        return solve(0, target, nums, memo);
     }
 
-    boolean f(int index, int target, int[] arr, int[][] dp){
+    private boolean solve(int i, int target, int[] nums, Boolean[][] dp) {
         if (target == 0) return true;
-        if (index == 0) return arr[0] == target;
+        if (i == nums.length) return false;
 
-        if (dp[index][target] != -1)
-            return dp[index][target] == 1;
+        if (dp[i][target] != null) return dp[i][target];
 
-        boolean notTake = f(index - 1, target, arr, dp);
-        boolean take = false;
-        if (arr[index] <= target)
-            take = f(index - 1, target - arr[index], arr, dp);
+        boolean notPick = solve(i + 1, target, nums, dp);
+        boolean pick = false;
+        if (nums[i] <= target) pick = solve(i + 1, target - nums[i], nums, dp);
 
-        dp[index][target] = (take || notTake) ? 1 : 0;
-        return dp[index][target] == 1;
+        return dp[i][target] = pick || notPick;
     }
 }
